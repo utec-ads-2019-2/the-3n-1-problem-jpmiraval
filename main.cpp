194b2ce3a86a5ee5ba8f156ac5d25f260bc8c3e7
jpmiraval <40766473+jpmiraval@users.noreply.github.com>
2019-08-22 14:42:26 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,13 +2,32 @@
 
 using namespace std;
 
+int contarCycle(int variable){
+    int cycleLength = 0;
+    while(variable != 1){
+        cycleLength++;
+        if(variable%2 == 0){
+            variable = variable/2;
+        }else{
+            variable = 3*variable + 1;
+        }
+    }
+    cycleLength++;
+    return cycleLength;
+}
+int esMayor(int valor, int maximo){
+    if(valor > maximo){
+        return valor;
+    }else{
+        return maximo;
+    }
+}
 int main() {
     int limiteInferior;
     int limiteSuperior;
     int cycleLength = 0;
     int temp;
     int maxCycleLength = 0;
-
     while(cin>> limiteInferior >> limiteSuperior){
         cout << limiteInferior << " " << limiteSuperior << " ";
         if(limiteInferior > limiteSuperior){
@@ -16,27 +35,13 @@ int main() {
             limiteInferior = limiteSuperior;
             limiteSuperior = temp;
         }
-        for(int i = limiteInferior; i < limiteSuperior+1; i++){
+        for(int i = limiteInferior; i < limiteSuperior+1; i++) {
             int Numero = i;
-            while(Numero != 1){
-                cycleLength++;
-                if(Numero%2 == 0){
-                    Numero = Numero/2;
-                }else{
-                    Numero = 3*Numero + 1;
-                }
-            }
-            cycleLength++;
-            if(cycleLength > maxCycleLength){
-                maxCycleLength = cycleLength;
-            }
-            cycleLength = 0;
+            cycleLength = contarCycle(Numero);
+            maxCycleLength = esMayor(cycleLength, maxCycleLength);
         }
 
         cout <<maxCycleLength << "\n";
-        maxCycleLength=0;
-    }
-
-
+        maxCycleLength=0;}
     return 0;
 }
